af828504d39a4ba8ac96e780d976125de64a37a5
James Ming <jamesming@gmail.com>
2012-07-17 07:21:20 -0700

    added

---
+++ 189/deploy/scripts/otherdata.js
@@ -0,0 +1,1 @@
+core.setOtherProperties({custom_html:'',about:'Eight years after Batman took the fall for Two Face&#039;s crimes, a new terrorist leader, Bane, overwhelms Gotham&#039;s finest, and the Dark Knight resurfaces to protect a city that has branded him an enemy'})
